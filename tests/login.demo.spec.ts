710cfec84e342049e2202d10c39a59e21397c934
GSNFonseka <nowanyafonseka@gmail.com>
2026-04-02 23:37:09 +0530

    Enable headed mode and add login actions

--- tests/login.demo.spec.ts
+++ tests/login.demo.spec.ts
@@ -5,6 +5,13 @@ test('login test', async ({ page} )=>{
     await page.goto('https://www.saucedemo.com/');
     
     const username: Locator = page.locator('#user-name');  // why here use locators instead of String 
+    const password: Locator = page.locator('#password');
+    const loginButton: Locator = page.locator('#login-button');
+
+    await username.fill('standard_user');
+    await password.fill('secret_sauce');
+    await loginButton.click();
+
     
 
 })
